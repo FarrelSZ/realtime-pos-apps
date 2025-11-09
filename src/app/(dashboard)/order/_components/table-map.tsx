7a2463938c6e51438d8527048505340b3a38d37f
FarrelSZ <farel.it12@gmail.com>
2025-11-09 14:09:42 +0700

    feat: create order from map

--- src/app/(dashboard)/order/_components/table-map.tsx
+++ src/app/(dashboard)/order/_components/table-map.tsx
@@ -1,17 +1,36 @@
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { HoverCard, HoverCardContent } from "@/components/ui/hover-card";
 import { cn } from "@/lib/utils";
 import { TableMapType } from "@/validations/table-validation";
-import { ReactFlow, Background, Controls } from "@xyflow/react";
+import { HoverCardTrigger } from "@radix-ui/react-hover-card";
+import { Background, ReactFlow } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { useMemo } from "react";
+import Link from "next/link";
+import { useMemo, useState } from "react";
+import DialogCreateOrderDineIn from "./dialog-create-order-dine-in";
 
-export function TableNode({ data }: { data: { label: string; capacity: number; status: string } }) {
+export function TableNode({
+  data,
+}: {
+  data: {
+    id: string;
+    label: string;
+    capacity: number;
+    status: string;
+    order?: {
+      order_id: string;
+      customer_name: string;
+    };
+  };
+}) {
+  const [openCreateOrder, setOpenCreateOrder] = useState(false);
   return (
     <HoverCard>
       <HoverCardTrigger asChild>
         <div
           className={cn(
-            "bg-muted rounded-lg flex items-center justify-center outline-2 outset-4 outline-dashed",
+            "bg-muted rounded-lg flex items-center justify-center outline-2 outline-offset-4 outline-dashed",
             {
               "w-20 h-20": data.capacity === 2,
               "w-32 h-20": data.capacity === 4,
@@ -29,29 +48,67 @@ export function TableNode({ data }: { data: { label: string; capacity: number; s
           {data.label}
         </div>
       </HoverCardTrigger>
-      <HoverCardContent className="mt-2 ">
+      <HoverCardContent className="mt-2">
         <div className="flex flex-col gap-2">
           <h4 className="text-sm font-semibold">Table {data.label}</h4>
           <p className="text-xs text-muted-foreground">Capacity: {data.capacity}</p>
           <p className="text-xs text-muted-foreground">Status: {data.status}</p>
+          {data.order ? (
+            <div className="flex flex-col gap-2">
+              <p className="text-xs text-muted-foreground">Order ID : {data.order.order_id}</p>
+              <p className="text-xs text-muted-foreground">Customer : {data.order.customer_name}</p>
+              <Link className="mt-2 w-full" href={`/order/${data.order.order_id}`}>
+                <Button>View Detail Order</Button>
+              </Link>
+            </div>
+          ) : (
+            <Dialog open={openCreateOrder} onOpenChange={setOpenCreateOrder}>
+              <DialogTrigger asChild>
+                <Button>Create Order</Button>
+              </DialogTrigger>
+              <DialogCreateOrderDineIn
+                closeDialog={() => setOpenCreateOrder(false)}
+                selectedTable={{
+                  id: data.id,
+                  name: data.label,
+                }}
+              />
+            </Dialog>
+          )}
         </div>
       </HoverCardContent>
     </HoverCard>
   );
 }
 
-export default function TableMap({ tables }: { tables: TableMapType[] }) {
+export default function TableMap({
+  tables,
+  activeOrders,
+}: {
+  tables: TableMapType[];
+  activeOrders: {
+    order_id: string;
+    customer_name: string;
+    tables: unknown;
+  }[];
+}) {
+  console.log(activeOrders);
   const nodeTypes = {
     tableNode: TableNode,
   };
+
   const initialNodes = useMemo(() => {
     return tables.map((table) => ({
       id: table.id,
       position: { x: table.position_x, y: table.position_y },
       data: {
+        id: table.id,
         label: table.name,
         capacity: table.capacity,
         status: table.status,
+        order: activeOrders.find((order) => {
+          return (order.tables as unknown as { id: string })?.id === table.id;
+        }),
       },
       type: "tableNode",
     }));
@@ -61,7 +118,6 @@ export default function TableMap({ tables }: { tables: TableMapType[] }) {
     <div className="w-[100%] h-[80vh] border rounded-lg">
       <ReactFlow proOptions={{ hideAttribution: true }} nodes={initialNodes} nodeTypes={nodeTypes}>
         <Background />
-        <Controls />
       </ReactFlow>
     </div>
   );
